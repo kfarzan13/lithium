941256a6437e0f94f4fceef7e6f983a6dbfec56b
kfarzan13 <kfarzan13@gmail.com>
2022-10-17 21:12:35 +0530

    Assignment on get-api done

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,6 +15,103 @@ router.get('/students', function (req, res){
 })
 
 
+
+
+
+
+// Problem No - 1
+
+router.get('/movies' , function(req , res) {
+    let movies = ["Rang de Basanti" , "The Shining" , "Lord of the Rings" , "Batman begins"]
+    console.log("Result :" , movies)
+    res.send(movies)
+})
+
+// Problem No - 2
+
+// router.get('/movies/:indexNumber' , function(req , res) {
+//     let movies = ["Rang de Basanti" , "The Shining" , "Lord of the Rings" , "Batman begins"]
+//     const index = req.params.indexNumber
+//     console.log(`Movie at index ${index} is :` , movies[index])
+//     res.send(movies[index])
+// })
+
+// Problem No - 3
+
+router.get('/movies/:indexNumber' , function(req , res) {
+    function check(){
+    let movies = ["Rang de Basanti" , "The Shining" , "Lord of the Rings" , "Batman begins"]
+    const index = req.params.indexNumber
+    if (index < movies.length) {
+        return movies[index]
+    } else { 
+        return `This index is not valid, Pls enter a valid index number.`
+    }
+    }
+    console.log("Result :", check())
+    res.send(check())
+})
+
+// Problem No - 4
+
+router.get('/films' , function(req , res) {
+    let films = [ {
+        "id": 1,
+        "name": "The Shining"
+        }, { "id": 2,
+        "name": "Conjuring-2"
+        }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+        }, {
+        "id": 4,
+        "name": "Finding Nemo"
+        }] 
+    console.log("Result :" , films)
+    res.send(films)
+})
+
+// Problem No - 5
+
+router.get('/films/:filmId' , function(req , res) {
+    function check(){
+
+        let films = [ 
+            {"id": 1,
+            "name": "The Shining"
+            }, { "id": 2,
+            "name": "Conjuring-2"
+            }, {
+            "id": 3,
+            "name": "Rang de Basanti"
+            }, {
+            "id": 4,
+            "name": "Finding Nemo"
+            } ] 
+
+
+    const id = req.params.filmId
+        for(let i=0 ; i < films.length ; i++){
+            let film = films[i]
+            if(film.id == id){
+                return film
+            } 
+        }
+        return "No film available with this Id."
+    }
+    console.log("Result :", check())
+    res.send(check())
+})
+
+
+
+
+
+
+
+
+
+
 // Example 1 for path params
 router.get('/students/:studentName', function(req, res){
     // ':' denotes that the following part of route is a variable
