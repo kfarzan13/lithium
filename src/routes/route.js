e103cddf9f6ab35e8395a046047ff25932085537
kfarzan13 <kfarzan13@gmail.com>
2022-10-20 20:09:55 +0530

    QueryParams Assignment Done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,36 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// // take marks in req.query in a variable named "marks" and send "pass" if marks>40 else send "fail"
-// router.get( "/get-query-2", function (req, res){
-//     let marks= req.query.marks
-//     let result = marks >40 ? "pass" : "fail"
-//     //ternary operator
-//     res.send( {data: result , status: true})
-// })
-
-// //query params ( as well as path params ) can be sent in post request as well
-// router.post( "/post-query-1", function (req, res){
-//     let data= req.query
-//     console.log( data)
-//     res.send( {data: data , status: true})
-// })
-
-// //filter out all the numbers that are greater than "input" ( input is received from query params)
-// let myArr = [23, 45, 67, 281394, 32424, 423, 24, 42323, 4, 234, 12, 34]
-
-// router.post( "/post-query-2", function (req, res){
-//     let input= req.query.input
-
-//     // let finArr= myArr.filter ( ele => ele>input )
-//     let finalArr= []
-//     for( i=0 ; i<myArr.length ; i++){
-//         if ( myArr[i] > input )     finalArr.push( myArr[i])
-//     }
-//     res.send( {data: finalArr , status: true})
-// })
-
-
 let persons= [
 {
     name: "PK",
@@ -62,22 +32,6 @@ let persons= [
 router.post('/person' , function(req , res) {
 
     let age = Number(req.query.votingAge)
-
-    // Method - 1
-
-    // for( i=0 ; i < persons.length ; i++) {
-    //     if ( persons[i].age >= age ) {
-    //         persons[i].votingStatus = true
-    //     }
-    // }
-
-    // persons.forEach(function(person){
-    //     if (person.age >= age) {
-    //         person.votingStatus = true;
-    //     }
-    // })
-
-    // Method - 2
 
     let eligiblePersons = []
 
